1c13f8e4fd7b9d2a311e18ef1f9e75c5eca12a9d
DiaSird <78157340+DiaSird@users.noreply.github.com>
2024-05-06 17:08:05 +0900

    feat: add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[allow(unused)]
+fn add() -> usize {
+    1 + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_add() {
+        let sum = add();
+        assert_eq!(sum, 2);
+    }
+}
